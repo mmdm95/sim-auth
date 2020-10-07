8b9ecc2d5e3c67758cd425904ac34f9576b2ddfd
MMDM <mmdehghan95@gmail.com>
2020-10-07 22:21:18 +0330

    remove constructor

--- src/DBAuth.php
+++ src/DBAuth.php
@@ -2,41 +2,14 @@
 
 namespace Sim\Auth;
 
-use PDO;
 use Sim\Auth\Abstracts\AbstractAuth;
-use Sim\Auth\Interfaces\IAuth;
-use Sim\Auth\Interfaces\IAuthException;
 
 class DBAuth extends AbstractAuth
 {
     /**
-     * @var PDO $pdo
-     */
-    protected $pdo;
-
-    /**
-     * DBAuth constructor.
-     * @param PDO $pdo_instance
-     * @param array $credentials
-     * @param array|null $config
-     * @param int $storage_type
-     * @throws IAuthException
-     */
-    public function __construct(
-        PDO $pdo_instance,
-        array $credentials,
-        ?array $config = null,
-        int $storage_type = IAuth::STORAGE_COOKIE
-    )
-    {
-        $this->pdo = $pdo_instance;
-        parent::__construct($credentials, $config, $storage_type);
-    }
-
-    /**
      * {@inheritdoc}
      */
-    public function login(array $credentials, string $extra_query = null)
+    public function login(string $extra_query = null)
     {
         return $this;
     }
