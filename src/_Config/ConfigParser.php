0c466c59c4c3febc195b5f439171091b756daa8c
MMDM <mmdehghan95@gmail.com>
2020-10-09 13:19:59 +0330

    implement methods

--- src/_Config/ConfigParser.php
+++ src/_Config/ConfigParser.php
@@ -2,12 +2,37 @@
 
 namespace Sim\Auth\Config;
 
+use PDO;
+use Sim\Auth\DB;
+use Sim\Auth\Exceptions\ConfigException;
+use Sim\Auth\Interfaces\IDBException;
+
 class ConfigParser
 {
+    /**
+     * @var DB
+     */
+    protected $db;
+
     /**
      * @var array
      */
     protected $config;
+
+    /**
+     * @var string
+     */
+    protected $credentials_key = 'credential_columns';
+
+    /**
+     * @var string
+     */
+    protected $credential_username_key = 'username';
+
+    /**
+     * @var string
+     */
+    protected $credential_password_key = 'password';
 
     /**
      * @var string
@@ -17,52 +42,135 @@ class ConfigParser
     /**
      * @var string
      */
-    protected $users_key = 'users';
+    protected $table_name_key = 'table_name';
 
     /**
      * @var string
      */
-    protected $roles_key = 'roles';
+    protected $columns_key = 'columns';
 
     /**
      * @var string
      */
-    protected $pages_key = 'pages';
+    protected $types_key = 'types';
 
     /**
      * @var string
      */
-    protected $user_role_key = 'user_role';
+    protected $constraints_key = 'constraints';
 
     /**
-     * @var string
+     * @var array
      */
-    protected $role_page_perm_key = 'role_page_perm';
+    protected $table_aliases = [
+        'users', 'roles', 'pages', 'user_role',
+        'role_page_perm', 'user_page_perm', 'sessions'
+    ];
 
     /**
-     * @var string
+     * @var array
      */
-    protected $user_page_perm_key = 'user_page_perm';
+    protected $tables = [];
 
     /**
-     * @var string
+     * @var array
      */
-    protected $sessions_key = 'sessions';
+    protected $structures = [];
+
+    /**
+     * @var array
+     */
+    protected $credentials = [];
 
     /**
      * ConfigParser constructor.
      * @param array $config
+     * @param PDO $pdo
+     * @throws IDBException
      */
-    public function __construct(array $config)
+    public function __construct(array $config, PDO $pdo)
     {
         $this->config = $config;
+        $this->parse();
+        $this->db = new DB($pdo);
     }
 
     /**
      * @return static
+     * @throws IDBException
      */
     public function up()
     {
+        if (!empty($this->structures)) {
+            // iterate through all tables for their structure
+            foreach ($this->structures as $tableName => $items) {
+                // create table is not exists
+                $this->db->createTableIfNotExists($tableName);
+                // iterate through all columns and create column if not exists
+                foreach ($items[$this->columns_key] as $columnName) {
+                    if (isset($items[$this->types_key][$columnName]) &&
+                        is_string($items[$this->types_key][$columnName]) &&
+                        !empty($items[$this->types_key][$columnName])) {
+                        $this->db->createColumnIfNotExists($tableName, $columnName, $items[$this->types_key][$columnName]);
+                    }
+                }
+                // iterate through all constraint and add it to table
+                foreach ($items[$this->constraints_key] as $constraint) {
+                    if (is_string($constraint) && !empty($constraint)) {
+                        $this->db->addConstraint($tableName, $constraint);
+                    }
+                }
+            }
+        }
         return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getTables(): array
+    {
+        return $this->tables;
+    }
+
+    public function getCredentialColumns(): array
+    {
+        return $this->credentials;
+    }
+
+    /**
+     * Parse config file
+     * @throws ConfigException
+     */
+    protected function parse(): void
+    {
+        if (!empty($this->config)) {
+            foreach ($this->config as $key => $structures) {
+                if ($key === $this->constraints_key) { // if it wat credentials columns key
+                    if (!isset($structures[$this->credential_username_key], $structures[$this->credential_password_key]) ||
+                        empty($structures[$this->credential_username_key]) || empty($structures[$this->credential_password_key])) {
+                        throw new ConfigException('Username/Password column is not set or it is empty!');
+                    }
+                    // store credential columns
+                    $this->credentials[$this->credential_username_key] = $structures[$this->credential_username_key];
+                    $this->credentials[$this->credential_password_key] = $structures[$this->credential_password_key];
+                } elseif ($key === $this->blueprint_key) { // if it was blueprints key
+                    // iterate through all blueprints
+                    foreach ($structures as $tableAlias => $structure) {
+                        // if alias is in aliases array
+                        if (in_array($tableAlias, $this->table_aliases)) {
+                            // get tables
+                            $this->tables[$tableAlias] = $structure[$this->table_name_key] ?? '';
+                            // get tables and other structure
+                            $this->structures[$tableAlias] = [
+                                $this->columns_key => $structure[$this->columns_key] ?? [],
+                                $this->types_key => $structure[$this->types_key] ?? [],
+                                $this->constraints_key => $structure[$this->constraints_key] ?? [],
+                            ];
+                        }
+                    }
+                }
+            }
+        }
     }
 }
