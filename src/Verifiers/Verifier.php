39f8373fd430d8f9980e2d207d21f35dd12f410b
MMDM <mmdehghan95@gmail.com>
2020-10-11 17:34:21 +0330

    implement password verification

--- src/Verifiers/Verifier.php
+++ src/Verifiers/Verifier.php
@@ -1,16 +1,34 @@
 <?php
 
-namespace Sim\Auth;
+namespace Sim\Auth\Verifiers;
 
 use Sim\Auth\Interfaces\IAuthVerifier;
 
 class Verifier implements IAuthVerifier
 {
     /**
+     * @var string|int
+     */
+    protected $algo;
+
+    /**
+     * Verifier constructor.
+     * @param $algo
+     */
+    public function __construct($algo)
+    {
+        $this->algo = $algo;
+    }
+
+    /**
      * {@inheritdoc}
      */
-    public function verify(string $text, $algorithm): bool
+    public function verify(string $text, string $hashed_value): bool
     {
-        
+        if (is_string($this->algo)) {
+            return hash($this->algo, $text) === $hashed_value;
+        } else {
+            return password_verify($text, $hashed_value);
+        }
     }
 }
