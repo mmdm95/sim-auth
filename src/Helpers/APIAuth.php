2c4930828ef3aeab553ba3ad92ed7bac5d3834b5
MMDM <mmdehghan95@gmail.com>
2020-10-18 07:23:18 +0330

    a new parsing function or api authentication with x_api_key header

---
+++ src/Helpers/APIAuth.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Sim\Auth\Helpers;
+
+class APIAuth
+{
+    /**
+     * It'll return the api key
+     *
+     * @return string
+     */
+    public static function parse(): string
+    {
+        $header = isset($_SERVER['HTTP_X_API_KEY'])
+            ? $_SERVER['HTTP_X_API_KEY']
+            : '';
+
+        return $header;
+    }
+}
